676f0d24c57e12d2e7f57b6e1d1590d65508b407
Marrya-beep <marrya.ah.2004@gmail.com>
2026-01-06 17:02:24 +0330

    Adding New Account in Login

---
+++ final/Shop/Shop/Controllers/CreateAccountController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models; 
+
+namespace Shop.Controllers
+{
+    public class CreateAccountController : Controller
+    {
+        private readonly ShopDbContext _context;
+
+        public CreateAccountController(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string userName, string email, string password)
+        {
+            if (_context.Users.Any(u => u.UserName == userName))
+            {
+                ViewBag.Error = "این نام کاربری قبلاً استفاده شده";
+                return View();
+            }
+
+            var user = new User
+            {
+                UserName = userName,
+                Email = email,
+                Password = password,
+                FKRole = 2,          
+                IsActive = true,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.Users.Add(user);
+            _context.SaveChanges();
+
+            ViewBag.Success = "اکانت شما با موفقیت ایجاد شد!";
+            return View();
+        }
+    }
+}
