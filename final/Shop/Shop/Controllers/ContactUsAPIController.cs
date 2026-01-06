bae61498be11a8d96b1af538a03ad704a6254141
Marrya-beep <marrya.ah.2004@gmail.com>
2026-01-06 10:55:09 +0330

    Adding Swagger ContactUs

---
+++ final/Shop/Shop/Controllers/ContactUsAPIController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Data;
+using Shop.Models;
+using Shop.Models.Dtos;
+
+
+namespace Shop.Controllers
+{
+
+    [ApiController]
+    [Route("api/contact")]
+    public class ContactUsAPIController : ControllerBase
+    {
+
+        private readonly ShopDbContext _context;
+
+        public ContactUsAPIController(ShopDbContext context)
+        {
+            _context = context;
+        }
+        [HttpPost("Send-Message")]
+        public IActionResult SendMessage([FromBody] ContactMessageDto dto)
+        {
+            var message = new ContactMessage
+            {
+                UserName = dto.UserName,
+                Phone = dto.Phone,
+                Email = dto.Email,
+                Message = dto.Message
+            };
+
+            _context.Message.Add(message);
+            _context.SaveChanges();
+
+            return Ok(new
+            {
+                success = true,
+                message = "پیام شما با موفقیت ارسال شد!"
+            });
+        }
+    }
+}
---
+++ final/Shop/Shop/Models/Dtos/ContactMessageDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Shop.Models.Dtos
+{
+    public class ContactMessageDto
+    {
+        public string UserName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
