6811accace29aa704e84fd465bd5d369b176012d
Marrya-beep <marrya.ah.2004@gmail.com>
2026-01-06 10:27:56 +0330

    Adding localhost

--- final/Shop/Shop/Program.cs
+++ final/Shop/Shop/Program.cs
@@ -17,11 +17,19 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy
-                .WithOrigins("http://127.0.0.1:5501")
+                .WithOrigins(
+                    "http://127.0.0.1:5500",
+                    "http://localhost:5500",
+                    "http://127.0.0.1:5501",
+                    "http://localhost:5501"
+
+
+                )
                 .AllowAnyHeader()
                 .AllowAnyMethod();
         });
 });
+
 
 var app = builder.Build();
 
